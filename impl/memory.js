5bfd5d9d48df4fa5438d50cc04c61aa73fa7a54a
clonq <ovi.comes@gmail.com>
2015-06-07 21:58:29 -0400

    complete v0 (crud) interface and in-memory implementation

--- impl/memory.js
+++ impl/memory.js
@@ -1,44 +1,68 @@
 var Promise = require('bluebird'),
-	schemaValidator = require('jsonschema'),
-	uuid = require('uuid'),
-	userSchema = require('bonsens-models').user,
-	buckets = {};
+    schemaValidator = require('jsonschema'),
+    uuid = require('uuid'),
+    _ = require('lodash'),
+    userSchema = require('bonsens-models').user,
+    buckets = {};
 
 module.exports = {
-	create: function (model) {
-		return new Promise(function(resolve, reject){
-        	var schemaErrors = schemaValidator.validate(model, userSchema).errors;
-        	if(schemaErrors.length == 0) {
-        		model.$id = uuid.v4();
-        		model.$created = Date.now();
-        		var bucket = model.$type || 'unknown';
-        		buckets[bucket] = {}
-        		buckets[bucket][model.$id] = model;
-        		return resolve(model);
-        	} else {
-				return reject(new Error(schemaErrors[0].stack));
-        	}
-		});
-	},
-	read: function (model) {
-		return new Promise(function(resolve, reject){
-        	var schemaErrors = schemaValidator.validate(model, userSchema).errors;
-        	if(schemaErrors.length == 0) {
-        		var bucket = model.$type || 'unknown';
-        		return resolve(buckets[bucket][model.$id]);
-        	} else {
-				return reject(new Error(schemaErrors[0].stack));
-        	}
-		});
-	},
-	update: function (model) {
-		return new Promise(function(resolve, reject){
-			return reject(new Error('not implemented'));
-		});
-	},
-	delete: function (model) {
-		return new Promise(function(resolve, reject){
-			return reject(new Error('not implemented'));
-		});
-	}
+    create: function (model) {
+        return new Promise(function(resolve, reject){
+            var schemaErrors = schemaValidator.validate(model, userSchema).errors;
+            if(schemaErrors.length == 0) {
+                model.$id = uuid.v4();
+                model.$created = Date.now();
+                var bucket = model.$type || 'unknown';
+                if(!buckets[bucket]) buckets[bucket] = {}
+                buckets[bucket][model.$id] = model;
+                return resolve(model);
+            } else {
+                return reject(new Error(schemaErrors[0].stack));
+            }
+        });
+    },
+    read: function (model) {
+        return new Promise(function(resolve, reject){
+            userSchema.required = ['$id'];
+            var schemaErrors = schemaValidator.validate(model, userSchema).errors;
+            if(schemaErrors.length == 0) {
+                var bucket = model.$type || 'unknown';
+                return resolve(buckets[bucket][model.$id]);
+            } else {
+                return reject(new Error(schemaErrors[0].stack));
+            }
+        });
+    },
+    update: function (model) {
+        return new Promise(function(resolve, reject){
+            userSchema.required = ['$id'];
+            var schemaErrors = schemaValidator.validate(model, userSchema).errors;
+            if(schemaErrors.length == 0) {
+                var bucket = model.$type || 'unknown';
+                var existingModel = buckets[bucket][model.$id];
+                var updatedModel = _.defaults(model, existingModel);
+                updatedModel.$updated = Date.now();
+                if(!buckets[bucket]) buckets[bucket] = {}
+                buckets[bucket][model.$id] = updatedModel;
+                return resolve(updatedModel);
+            } else {
+                return reject(new Error(schemaErrors[0].stack));
+            }
+        });
+    },
+    delete: function (model) {
+        return new Promise(function(resolve, reject){
+            userSchema.required = ['$id'];
+            var schemaErrors = schemaValidator.validate(model, userSchema).errors;
+            if(schemaErrors.length == 0) {
+                var bucket = model.$type || 'unknown';
+                var deletedModel = buckets[bucket][model.$id];
+                deletedModel.$deleted = Date.now();
+                delete buckets[bucket][model.$id];
+                return resolve(deletedModel);
+            } else {
+                return reject(new Error(schemaErrors[0].stack));
+            }
+        });
+    }
 }
--- test/api.js
+++ test/api.js
@@ -1,72 +1,111 @@
 var debug = require('debug')('dao:test'),
-	should = require('chai').should()
-	dao = require('../index');
-	memoryImpl = require('../impl/memory');
+    should = require('chai').should()
+    dao = require('../index');
+    memoryImpl = require('../impl/memory');
 
 const TEST_MODEL = {
-	name: 'joe',
-	age: 23
+    name: 'joe',
+    age: 23
 };
 
 var TEST_ID;
+var TEST_EMAIL = 'joe@test.com';
 
-describe("api tests", function() {
-	it('should fail if there no implementation provided', function(done){
-		dao
-		.create(TEST_MODEL)
-		.then(function(res){
-			debug(res)
-			done();
-		})
-		.catch(function(err){
-			should.exist(err);
-			err.should.have.property('message');
-			err.message.should.equal('No DAO implementation available');
-			done();
-		})
-	});
-	it('should validate implementations conforming to the dao schema v0', function(done){
-		try {
-			dao.use(memoryImpl);
-			done();
-		} catch(err) {
-			done(err)
-		}
-	});
-	it('should return a model with an id on create', function(done){
-		try {
-			dao
-			.use(memoryImpl)
-			.create(TEST_MODEL)
-			.then(function(createdModel){
-				should.exist(createdModel);
-				createdModel.should.have.property('$id');
-				TEST_ID = createdModel.$id;
-				done();
-			})
-			.catch(function(err){
-				done(err);
-			})
-		} catch(err) {
-			done(err)
-		}
-	});
-	it('should return a model based on id on read', function(done){
-		try {
-			TEST_MODEL.$id = TEST_ID;
-			dao
-			.use(memoryImpl)
-			.read(TEST_MODEL)
-			.then(function(foundModel){
-				should.exist(foundModel);
-				foundModel.should.have.property('$id').and.equal(TEST_ID);
-				done();
-			})
-			.catch(function(err){
-				done(err);
-			})
-		} catch(err) {
-			done(err)
-		}
-	});
+describe("v0 api tests", function() {
+    it('should fail if there no implementation provided', function(done){
+        dao
+        .create(TEST_MODEL)
+        .then(function(res){
+            debug(res)
+            done();
+        })
+        .catch(function(err){
+            should.exist(err);
+            err.should.have.property('message');
+            err.message.should.equal('No DAO implementation available');
+            done();
+        })
+    });
+    it('should validate implementations conforming to the dao schema v0', function(done){
+        try {
+            dao.use(memoryImpl);
+            done();
+        } catch(err) {
+            done(err)
+        }
+    });
+    it('should return a newly created model', function(done){
+        try {
+            dao
+            .use(memoryImpl)
+            .create(TEST_MODEL)
+            .then(function(createdModel){
+                should.exist(createdModel);
+                createdModel.should.have.property('$id');
+                TEST_ID = createdModel.$id;
+                done();
+            })
+            .catch(function(err){
+                done(err);
+            })
+        } catch(err) {
+            done(err)
+        }
+    });
+    it('should return an existing model', function(done){
+        try {
+            TEST_MODEL.$id = TEST_ID;
+            dao
+            .use(memoryImpl)
+            .read(TEST_MODEL)
+            .then(function(foundModel){
+                should.exist(foundModel);
+                foundModel.should.have.property('$id').and.equal(TEST_ID);
+                done();
+            })
+            .catch(function(err){
+                done(err);
+            })
+        } catch(err) {
+            done(err)
+        }
+    });
+    it('should return a valid updated model', function(done){
+        try {
+            TEST_MODEL.$id = TEST_ID;
+            TEST_MODEL.email = TEST_EMAIL;
+            dao
+            .use(memoryImpl)
+            .update(TEST_MODEL)
+            .then(function(updatedModel){
+                should.exist(updatedModel);
+                updatedModel.should.have.property('email').and.equal(TEST_EMAIL);
+                updatedModel.should.have.property('$updated');
+                done();
+            })
+            .catch(function(err){
+                done(err);
+            })
+        } catch(err) {
+            done(err)
+        }
+    });
+    it('should return the deleted model', function(done){
+        try {
+            TEST_MODEL.$id = TEST_ID;
+            dao
+            .use(memoryImpl)
+            .delete(TEST_MODEL)
+            .then(function(deletedModel){
+                should.exist(deletedModel);
+                deletedModel.should.have.property('$deleted');
+                done();
+            })
+            .catch(function(err){
+                done(err);
+            })
+        } catch(err) {
+            done(err)
+        }
+    });
 });
