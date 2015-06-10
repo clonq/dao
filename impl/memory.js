2a4404c7d89059ead96e6dc18fee3001b869d6bc
clonq <ovi.comes@gmail.com>
2015-06-09 21:13:25 -0400

    refactored in-memory validator

--- impl/memory.js
+++ impl/memory.js
@@ -1,44 +1,32 @@
 var Promise = require('bluebird'),
-    schemaValidator = require('jsonschema'),
     uuid = require('uuid'),
     _ = require('lodash'),
-    userSchema = require('bonsens-models').user,
     buckets = {};
 
 module.exports = {
     create: function (model) {
         return new Promise(function(resolve, reject){
-            userSchema.required = [];
-            var schemaErrors = schemaValidator.validate(model, userSchema).errors;
-            if(schemaErrors.length == 0) {
-                model.$id = uuid.v4();
-                model.$created = Date.now();
-                var bucket = model.$type || 'unknown';
-                if(!buckets[bucket]) buckets[bucket] = {}
-                buckets[bucket][model.$id] = model;
-                return resolve(model);
-            } else {
-                return reject(new Error(schemaErrors[0].stack));
-            }
+            model.$id = uuid.v4();
+            model.$created = Date.now();
+            var bucket = model.$type || 'unknown';
+            if(!buckets[bucket]) buckets[bucket] = {};
+            buckets[bucket][model.$id] = model;
+            return resolve(model);
         });
     },
     read: function (model) {
         return new Promise(function(resolve, reject){
-            userSchema.required = ['$id'];
-            var schemaErrors = schemaValidator.validate(model, userSchema).errors;
-            if(schemaErrors.length == 0) {
+            if(model && model.$id) {
                 var bucket = model.$type || 'unknown';
                 return resolve(buckets[bucket][model.$id]);
             } else {
-                return reject(new Error(schemaErrors[0].stack));
+                return reject(new Error('required $id is missing'));
             }
         });
     },
     update: function (model) {
         return new Promise(function(resolve, reject){
-            userSchema.required = ['$id'];
-            var schemaErrors = schemaValidator.validate(model, userSchema).errors;
-            if(schemaErrors.length == 0) {
+            if(model && model.$id) {
                 var bucket = model.$type || 'unknown';
                 var existingModel = buckets[bucket][model.$id];
                 var updatedModel = _.defaults(model, existingModel);
@@ -47,22 +35,20 @@ module.exports = {
                 buckets[bucket][model.$id] = updatedModel;
                 return resolve(updatedModel);
             } else {
-                return reject(new Error(schemaErrors[0].stack));
+                return reject(new Error('required $id is missing'));
             }
         });
     },
     delete: function (model) {
         return new Promise(function(resolve, reject){
-            userSchema.required = ['$id'];
-            var schemaErrors = schemaValidator.validate(model, userSchema).errors;
-            if(schemaErrors.length == 0) {
+            if(model && model.$id) {
                 var bucket = model.$type || 'unknown';
                 var deletedModel = buckets[bucket][model.$id];
                 deletedModel.$deleted = Date.now();
                 delete buckets[bucket][model.$id];
                 return resolve(deletedModel);
             } else {
-                return reject(new Error(schemaErrors[0].stack));
+                return reject(new Error('required $id is missing'));
             }
         });
     }
